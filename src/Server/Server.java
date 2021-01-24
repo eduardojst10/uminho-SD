9d1d09347fcc67ad6a6eb6f38644545466859b14
karatebraga <74617557+karatebraga@users.noreply.github.com>
2021-01-24 17:40:29 +0000

    server sem workers

--- src/Server/Server.java
+++ src/Server/Server.java
@@ -6,8 +6,6 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
-    final static int WORKERS_PER_CONNECTION = 3;
-
     public static void main(String[] args) throws Exception {
         ServerSocket ss = new ServerSocket(12345);
         System.out.println("--------- SD SERVER 2021 ---------");
@@ -18,10 +16,8 @@ public class Server {
             Socket socket = ss.accept();
             System.out.println("--------- CONNECTION " + socket.getPort() + " ESTABLISHED ---------");
 
-            for (int i = 0; i < WORKERS_PER_CONNECTION; ++i)
-                new Thread(new ResponseWorker(socket, app)).start();
+            new Thread(new ResponseWorker(socket, app)).start();
         }
     }
 
 }
-
